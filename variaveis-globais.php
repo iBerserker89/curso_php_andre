319cb5bf5285f49f2bdda81b51e055fbf574fb46
Luciano Barros <lucianof.barros89@gmail.com>
2024-06-28 14:15:04 -0300

    Enviando variaveis globais

--- variaveis-globais.php
+++ variaveis-globais.php
@@ -1,1 +1,13 @@
 <?php
+
+// Só usar caso seja extremamente necessário.
+
+$texto = 'oi';
+function imprime_texto() {
+    global $texto;
+    $texto = 'Oi mundo';
+}
+
+imprime_texto();
+
+echo $texto;
