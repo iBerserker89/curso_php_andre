86d5bfd5103107bf5e8c10c7ffe827411484276a
Luciano Barros <lucianof.barros89@gmail.com>
2024-06-28 14:23:55 -0300

    enviando argumentos

---
+++ argumentos.php
@@ -0,0 +1,15 @@
+<?php
+
+function foo() {
+    $numero_de_argumentos = func_num_args();
+    echo 'Número de argumentos ' . $numero_de_argumentos . '<br>';
+    if ($numero_de_argumentos >= 2) {
+        echo 'O segundo argumento é ' . func_get_arg(1) . '<br>';
+    }
+    $argumentos = func_get_args();
+    foreach ($argumentos as $variavel => $valor) {
+        echo "Argumento " . $variavel . ' é: ' . $valor . '<br>';
+    }
+}
+
+foo(1, 'Olá mundo', 3, 2.5, true);
